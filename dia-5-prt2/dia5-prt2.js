3c415aa0dcd5269101e520219592ef3293f603e0
José Pedro Rodrigues <programadorrodriguess@gmail.com>
2025-02-24 21:52:23 -0300

    correção das atividades

--- dia-5-prt2/dia5-prt2.js
+++ dia-5-prt2/dia5-prt2.js
@@ -1,18 +1,21 @@
 // CRIANDO VARIAVEIS
 
 let opções = prompt('O que você deseja:' + '\n1 = abastecer com gasolina; \n2 = abastecer com álcool; \n3 = calibrar os pneus.')
-
+let quantidade
+let valor 
 // SWITCHCASE
 
 switch (opções) {
 
     case '1':
-        let valor = parseInt(prompt('Qual será o valor?'))
-        console.log('A quantidade abastecida de gasolina foi de: ' + (valor / 5 + ' litros'))
+        valor = parseInt(prompt('Qual será o valor?'))
+        quantidade = valor / 5
+        console.log('A quantidade abastecida de gasolina foi de: ' + quantidade + ' litros')
         break;
     case '2':    
-        let valor2 = parseInt(prompt('Qual será o valor?'))
-        console.log('A quantidade abastecida de álcool foi de: ' + (valor2 / 3 + ' litros.'))
+        valor = parseInt(prompt('Qual será o valor?'))
+        quantidade = valor / 3 
+        console.log('A quantidade abastecida de álcool foi de: ' + quantidade + ' litros.')
         break;
     case '3':
         console.log('pneus calibrados com sucesso!');
