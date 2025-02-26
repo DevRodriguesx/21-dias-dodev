346ae2b8ee36c61ff41ee808c740e4a6dc828f74
José Pedro Rodrigues <programadorrodriguess@gmail.com>
2025-02-25 21:54:42 -0300

    algoritmo utilizando a estrutura de repetição FOR que recebe 1 número e exibe na tela qual o resultado da sua tabuada e dos próximos 2 números

---
+++ dia-6-prt4/dia6-prt4.js
@@ -0,0 +1,14 @@
+// VARIAVEL
+let numero = Number(prompt('Escolha um número no qual deseja ver sua tabuada'))
+
+// FOR
+
+for (let tabuada = 0; tabuada <= 10; tabuada++){
+        console.log(numero + ' x ' + tabuada + ' = ' + numero * tabuada);
+}
+for (let tabuada = 0; tabuada <= 10; tabuada++){
+        console.log(numero + 1 + ' x ' + tabuada + ' = ' + (numero + 1) * tabuada);
+}
+for (let tabuada = 0; tabuada <= 10; tabuada++){
+        console.log(numero + 2 + ' x ' + tabuada + ' = ' + (numero + 2) * tabuada);
+}
